40accda96447b63be4df707f5f6dab83fe184af8
Talison Fabio <54823205+talis-fb@users.noreply.github.com>
2023-01-16 19:40:09 -0300

    feat: Add Validators to show Response json pretty

--- src/base/validators/response.rs
+++ src/base/validators/response.rs
@@ -1,19 +1,42 @@
-// use super::Validator;
-// use crate::base::web::request::Request;
-//
-// #[derive(Default)]
-// pub struct ValidUrlProtocolRequest;
-//
-// impl IValidator<Request> for ValidUrlProtocolRequest {
-//     fn execute(&self, value: &mut Request) -> Result<(), String> {
-//         let has_the_protocol_in_begin =
-//             regex::Regex::new(r"^((http|https)://)(.+)$").map_err(|e| e.to_string())?;
-//
-//         if !has_the_protocol_in_begin.is_match(&value.url) {
-//             let protocol = "http://".to_string();
-//             value.url = protocol + &value.url;
-//         }
-//
-//         Ok(())
-//     }
-// }
+use super::{Validator, Validators};
+use crate::base::web::response::Response;
+use serde_json::Value;
+
+impl Validators {
+    pub fn set_pretty_json_response() -> Validator<Response> {
+        |res: &mut Response| {
+            let json_obj: Value = serde_json::from_str(&res.body).map_err(|e| e.to_string())?;
+            let prety_body = serde_json::to_string_pretty(&json_obj).map_err(|e| e.to_string())?;
+            res.body = prety_body;
+            Ok(())
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::ValidatorsHandler;
+    use super::*;
+
+    #[test]
+    fn should_format_string_of_response_body() {
+        let mut response = Response::default();
+        response.body = r#"{ "StatusCode": "200", "SomeMessenger": "Here's something interesting", "Notes": ["Here live anothers interesting things"] }"#.to_string();
+
+        let response_final = ValidatorsHandler::from(response.clone())
+            .execute(vec![Validators::set_pretty_json_response()])
+            .unwrap();
+
+        assert_eq!(
+            response_final.body,
+            r#"{
+  "Notes": [
+    "Here live anothers interesting things"
+  ],
+  "SomeMessenger": "Here's something interesting",
+  "StatusCode": "200"
+}"#
+            .to_string()
+        );
+    }
+}
