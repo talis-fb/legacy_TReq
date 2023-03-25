d0cbe8d55941274cd501e859e1c7e967eed87d68
Talison Fabio <54823205+talis-fb@users.noreply.github.com>
2023-03-25 10:30:10 -0300

    fix: Bug in doc view when user goes beyond limit of text

--- src/base/doc/handler.rs
+++ src/base/doc/handler.rs
@@ -21,3 +21,111 @@ impl DocReaderHandler {
         self.position
     }
 }
+
+// -----------------------------
+// Position manipulation -------
+// -----------------------------
+impl DocReaderHandler {
+    pub fn go_to_next_row(&mut self) {
+        let last_row_index = self.doc.get_number_rows() - 1;
+        self.position = std::cmp::min(last_row_index, self.position + 1);
+    }
+
+    pub fn go_to_prev_row(&mut self) {
+        if self.position > usize::MIN {
+            self.position -= 1;
+        }
+     }
+
+    pub fn go_to_start(&mut self) {
+        self.position = 0;
+    }
+
+    pub fn go_to_end(&mut self) {
+        self.position = self.doc.get_number_rows() - 1;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_goes_up_and_down_corretly() {
+        let data = r#"
+        {
+            "content": [
+                [ ["Row 1", null] ],
+                [ ["Row 2", null] ],
+                [ ["Row 3", null] ],
+                [ ["Row 4", null] ],
+                [ ["Row 5", null] ],
+                [ ["Row 6", null] ],
+                [ ["Row 7", null] ],
+                [ ["Row 8", null] ]
+            ]
+        }"#;
+
+        let mut handler = DocReaderHandler::init(DocView::from_string(data.to_string()));
+
+        assert_eq!(handler.position, 0);
+
+        handler.go_to_next_row();
+
+        assert_eq!(handler.position, 1);
+
+        handler.go_to_next_row();
+        handler.go_to_next_row();
+
+        assert_eq!(handler.position, 3);
+
+        for _ in 0..100  {
+            handler.go_to_next_row();
+        }
+
+        assert_eq!(handler.position, 7);
+
+        handler.go_to_prev_row();
+
+        assert_eq!(handler.position, 6);
+
+        handler.go_to_prev_row();
+        handler.go_to_prev_row();
+
+        assert_eq!(handler.position, 4);
+
+        for _ in 0..100  {
+            handler.go_to_prev_row();
+        }
+
+        assert_eq!(handler.position, 0);
+    }
+
+    #[test]
+    fn should_goes_start_and_end_corretly() {
+        let data = r#"
+        {
+            "content": [
+                [ ["Row 1", null] ],
+                [ ["Row 2", null] ],
+                [ ["Row 3", null] ],
+                [ ["Row 4", null] ],
+                [ ["Row 5", null] ],
+                [ ["Row 6", null] ],
+                [ ["Row 7", null] ],
+                [ ["Row 8", null] ]
+            ]
+        }"#;
+
+        let mut handler = DocReaderHandler::init(DocView::from_string(data.to_string()));
+
+        handler.go_to_next_row();
+        handler.go_to_next_row();
+
+        handler.go_to_start();
+        assert_eq!(handler.position, 0);
+
+        handler.go_to_end();
+        assert_eq!(handler.position, 7);
+    }
+}
