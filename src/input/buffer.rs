9e36b0be67c80d5a6c92ee27f3e93aaa198a31e7
Talison Fabio <54823205+talis-fb@users.noreply.github.com>
2023-03-25 09:14:20 -0300

    test: Testing cases of InputKeyboardBuffer (use of cursor)

--- src/input/buffer.rs
+++ src/input/buffer.rs
@@ -139,3 +139,160 @@ impl InputKeyboardBuffer {
     // pub fn DeleteLine() {
     // }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_init_and_be_defined() {
+        let buffer = InputKeyboardBuffer::init();
+
+        assert_eq!(buffer.value, "".to_string());
+        assert_eq!(buffer.get_cursor(), 0);
+    }
+
+    #[test]
+    fn should_execute_corretly_set_cursor() {
+        let mut buffer = InputKeyboardBuffer::init();
+
+        buffer.set_value("Hello World".to_string());
+
+        // Hello World<>
+        assert_eq!(buffer.get_cursor(), 11);
+
+        buffer.set_cursor(2);
+
+        // He<l>lo World
+        assert_eq!(buffer.value, "Hello World".to_string());
+        assert_eq!(buffer.get_cursor(), 2);
+
+        buffer.set_cursor(33);
+
+        // Hello World<>
+        assert_eq!(buffer.value, "Hello World".to_string());
+        assert_eq!(buffer.get_cursor(), 11);
+
+        buffer.set_cursor(6);
+
+        // Hello <W>orld
+        assert_eq!(buffer.value, "Hello World".to_string());
+        assert_eq!(buffer.get_cursor(), 6);
+    }
+
+    #[test]
+    fn should_change_cursor_with_set_value() {
+        let mut buffer = InputKeyboardBuffer::init();
+        buffer.set_value("Hello World".to_string());
+        assert_eq!(buffer.get_cursor(), 11);
+
+        buffer.set_value("Another Value with length 28".to_string());
+
+        assert_eq!(buffer.get_cursor(), 28);
+    }
+
+    #[test]
+    fn should_insert_corretly() {
+        let mut buffer = InputKeyboardBuffer::init();
+        buffer.set_value("Hello World".to_string());
+
+        buffer.insert_char('X');
+
+        assert_eq!(buffer.value, "Hello WorldX".to_string());
+        assert_eq!(buffer.get_cursor(), 12);
+    }
+
+    #[test]
+    fn should_delete_corretly() {
+        let mut buffer = InputKeyboardBuffer::init();
+        buffer.set_value("Hello World".to_string());
+
+        buffer.delete_prev_char();
+
+        assert_eq!(buffer.value, "Hello Worl".to_string());
+        assert_eq!(buffer.get_cursor(), 10);
+
+
+        // Hel<l>o Worl
+        buffer.set_cursor(3);
+
+
+        // He<l>o Worl
+        buffer.delete_prev_char();
+        assert_eq!(buffer.value, "Helo Worl".to_string());
+        assert_eq!(buffer.get_cursor(), 2);
+
+
+        // He<o> World
+        buffer.delete_next_char();
+        assert_eq!(buffer.value, "Heo Worl".to_string());
+        assert_eq!(buffer.get_cursor(), 2);
+
+
+        // He<>
+        buffer.delete_till_end();
+        assert_eq!(buffer.value, "He".to_string());
+        assert_eq!(buffer.get_cursor(), 2);
+    }
+
+    #[test]
+    fn should_jump_corretly() {
+        let mut buffer = InputKeyboardBuffer::init();
+        buffer.set_value("Hello World".to_string());
+
+        fn is_value_same_initial(buffer: &InputKeyboardBuffer){
+            assert_eq!(buffer.value, "Hello World".to_string());
+        }
+
+        // Hello World<>
+        is_value_same_initial(&buffer);
+        assert_eq!(buffer.get_cursor(), 11);
+
+
+        // Hello Worl<d>
+        buffer.go_to_prev_char();
+        is_value_same_initial(&buffer);
+        assert_eq!(buffer.get_cursor(), 10);
+
+
+        // Hello Wo<r>ld
+        buffer.go_to_prev_char();
+        buffer.go_to_prev_char();
+        is_value_same_initial(&buffer);
+        assert_eq!(buffer.get_cursor(), 8);
+
+
+        // Hello Wor<l>d
+        buffer.go_to_next_char();
+        is_value_same_initial(&buffer);
+        assert_eq!(buffer.get_cursor(), 9);
+
+
+        // Hello World<>
+        for _ in 0..100 {
+            buffer.go_to_next_char();
+        }
+        is_value_same_initial(&buffer);
+        assert_eq!(buffer.get_cursor(), 11);
+
+
+        // <H>ello World
+        for _ in 0..100 {
+            buffer.go_to_prev_char();
+        }
+        is_value_same_initial(&buffer);
+        assert_eq!(buffer.get_cursor(), 0);
+
+
+        // Hello World<>
+        buffer.go_to_end();
+        is_value_same_initial(&buffer);
+        assert_eq!(buffer.get_cursor(), 11);
+
+
+        // <H>ello Worl
+        buffer.go_to_start();
+        is_value_same_initial(&buffer);
+        assert_eq!(buffer.get_cursor(), 0);
+    }
+}
