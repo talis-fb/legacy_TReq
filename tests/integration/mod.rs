6f96380deff48f5b3e0e779abee7c28cab26066f
talis-fb <talisonf11@gmail.com>
2023-04-08 21:31:43 -0300

    fixing Dockerfiles and Add Dockerfile to integration tests

---
+++ tests/integration/mod.rs
@@ -0,0 +1,17 @@
+#[cfg(test)]
+mod file_handler_test {
+    #[test]
+    #[ignore]
+    fn sgodasdasdsa() {
+        assert_eq!(4, 4)
+    }
+}
+
+#[cfg(test)]
+mod network {
+    #[test]
+    #[ignore]
+    fn sgodasdasdsa() {
+        assert_eq!(4, 4)
+    }
+}
