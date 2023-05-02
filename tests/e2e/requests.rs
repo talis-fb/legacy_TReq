cc3ff117f7a2926173c907ba074034b71ef623a8
Talison Fabio <54823205+talis-fb@users.noreply.github.com>
2023-05-02 08:47:25 -0300

    test: Testing set body and headers request with external Editor

--- tests/e2e/requests.rs
+++ tests/e2e/requests.rs
@@ -1,20 +1,15 @@
+use std::collections::HashMap;
+
 use crate::mocks::file_factory::MockFileFactory;
 use crate::mocks::mock_app::MockApp;
-use crate::utils::set_input_mode_value;
+use crate::utils::{set_external_editor_output, set_input_mode_value};
 use treq::app::InputMode;
 use treq::base::actions::Actions;
-use treq::base::os::os_commands::factory::MockOsCommandFactory;
-use treq::base::os::os_commands::MockOsCommandTrait;
 use treq::base::web::request::Request;
 use treq::input::input_handler::MockInputHandler;
 
 #[tokio::test]
-async fn should_edit_body_request() {
-    let mut mock_app = MockApp::init();
-}
-
-#[tokio::test]
-async fn should_edit_header_request() {
+async fn should_edit_body_and_header_request() {
     let mut mock_app = MockApp::init();
 
     mock_app.exec(Actions::GoToRequestBody).await;
@@ -22,7 +17,7 @@ async fn should_edit_header_request() {
     let req_default = Request::default();
 
     assert_eq!(
-        "{}",
+        req_default.body,
         mock_app.runner.app.get_data_store().get_request().body
     );
     assert_eq!(
@@ -30,28 +25,49 @@ async fn should_edit_header_request() {
         mock_app.runner.app.get_data_store().get_request().headers
     );
 
-    // let mut file_factory = MockFileFactory::default();
-
     let mut input_handler = MockInputHandler::default();
     input_handler.expect_update().returning(|_| {});
     input_handler.expect_close_async_listener().returning(|| {});
-
-    // let mut path_called_to_body = Rc
-
-    let mut os_command_factory = MockOsCommandFactory::default();
-    os_command_factory
-        .expect_external_editor()
-        .return_once(move |_, _| {
-            // path_called_to_body.push(path);
-            Ok(Box::new(MockOsCommandTrait::default()))
-        });
-
-    // Set mocks on app
-    mock_app
-        .runner
-        .app
-        .set_os_commands_factory(os_command_factory);
     mock_app.runner.input_handler = input_handler;
 
-    // mock_app.exec(Actions::Edit).await;
+    let req = mock_app.runner.app.get_data_store().get_request();
+    assert_eq!(req.body, req_default.body);
+
+    set_external_editor_output(&mut mock_app, String::from(r#"{ "my_body": "somethin" }"#)).await;
+    mock_app.exec(Actions::Edit).await;
+
+    let req = mock_app.runner.app.get_data_store().get_request();
+    assert_eq!(req.body, r#"{ "my_body": "somethin" }"#);
+    assert_eq!(req.headers, req_default.headers);
+
+    mock_app.exec(Actions::Switch).await;
+
+    set_external_editor_output(
+        &mut mock_app,
+        String::from(r#"{ "some_other_header": "something" }"#),
+    )
+    .await;
+    mock_app.exec(Actions::Edit).await;
+
+    let req = mock_app.runner.app.get_data_store().get_request();
+    assert_eq!(req.body, r#"{ "my_body": "somethin" }"#);
+    assert_eq!(
+        req.headers,
+        HashMap::from([("some_other_header".to_string(), "something".to_string())])
+    );
+
+    set_external_editor_output(
+        &mut mock_app,
+        String::from(r#"{ some_invalid_json: _something_ }"#),
+    )
+    .await;
+    mock_app.exec(Actions::Edit).await;
+
+    // Doens't change
+    let req = mock_app.runner.app.get_data_store().get_request();
+    assert_eq!(req.body, r#"{ "my_body": "somethin" }"#);
+    assert_eq!(
+        req.headers,
+        HashMap::from([("some_other_header".to_string(), "something".to_string())])
+    );
 }
--- tests/utils/mod.rs
+++ tests/utils/mod.rs
@@ -1,5 +1,5 @@
 use crate::mocks::mock_app::MockApp;
-use treq::base::actions::Actions;
+use treq::base::{actions::Actions, os::os_commands::{MockOsCommandTrait, factory::MockOsCommandFactory}, commands::Commands};
 
 pub async fn set_input_mode_value(mock_app: &mut MockApp, value: &str) {
     mock_app.exec(Actions::TypingClearAll).await;
@@ -9,4 +9,35 @@ pub async fn set_input_mode_value(mock_app: &mut MockApp, value: &str) {
     }
 
     mock_app.exec(Actions::TypingClose).await;
+}
+
+pub async fn set_external_editor_output(mock_app: &mut MockApp, value: String) {
+    let mut os_command_factory = MockOsCommandFactory::default();
+    os_command_factory
+        .expect_external_editor()
+        .return_once(move |_, command| {
+            let mut editor_to_edit_body = MockOsCommandTrait::default();
+            editor_to_edit_body
+                .expect_exec()
+                .times(1)
+                .returning(move |sender| {
+                    let set_input_buffer = Commands::set_input_buffer(value.clone());
+                    let exec_input_buffer = command.clone();
+
+                    tokio::task::spawn(async move {
+                        sender.send(set_input_buffer).await.ok();
+                        sender.send(exec_input_buffer).await.ok();
+                    });
+
+                    Ok(())
+                });
+
+            Ok(Box::new(editor_to_edit_body))
+        });
+
+    // Set mocks on app
+    mock_app
+        .runner
+        .app
+        .set_os_commands_factory(os_command_factory);
 }
